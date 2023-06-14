7b1541bdcd7f0d201d14f1744b85d48c42b3b9df
cferro <carlosferro@gmail.com>
2023-06-14 17:20:38 +0200

    Deserialize password. Don't serialize password.

--- src/main/java/com/ferro/termin/user/User.java
+++ src/main/java/com/ferro/termin/user/User.java
@@ -1,115 +1,115 @@
 package com.ferro.termin.user;
 
-import java.util.Collection;
-import java.util.Objects;
-
 import com.fasterxml.jackson.annotation.JsonIgnore;
-
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import jakarta.persistence.Inheritance;
-import jakarta.persistence.InheritanceType;
 import jakarta.persistence.Table;
+
+import java.util.Collection;
+import java.util.Objects;
 
 @Entity
 @Table(name = "users")
 //@Inheritance(strategy = InheritanceType.JOINED)
 public class User {
-	@Id
-	@GeneratedValue
-	private Integer id;
-	@JsonIgnore
-	private String password;
-	private String name;
-	private String lastName;
-	private String email;
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String password;
+    private String name;
+    private String lastName;
+    private String email;
 
-	private Collection<Role> roles;
+    private Collection<Role> roles;
 
-	public User() {
-		super();
-	}
+    public User() {
+        super();
+    }
 
-	public User(Integer id, String password, String name, String lastName, String email, Collection<Role> roles) {
-		super();
-		this.id = id;
-		this.password = password;
-		this.name = name;
-		this.lastName = lastName;
-		this.email = email;
-		this.roles = roles;
-	}
+    public User(Integer id, String password, String name, String lastName, String email, Collection<Role> roles) {
+        super();
+        this.id = id;
+        this.password = password;
+        this.name = name;
+        this.lastName = lastName;
+        this.email = email;
+        this.roles = roles;
+    }
 
-	public Integer getId() {
-		return id;
-	}
+    public Integer getId() {
+        return id;
+    }
 
-	public void setId(Integer id) {
-		this.id = id;
-	}
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	public String getLastName() {
-		return lastName;
-	}
+    public String getLastName() {
+        return lastName;
+    }
 
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 
-	public String getEmail() {
-		return email;
-	}
+    public String getEmail() {
+        return email;
+    }
 
-	public void setEmail(String email) {
-		this.email = email;
-	}
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
-	@Override
-	public int hashCode() {
-		return Objects.hash(email, id);
-	}
+    @Override
+    public int hashCode() {
+        return Objects.hash(email, id);
+    }
 
-	public String getPassword() {
-		return password;
-	}
 
-	public void setPassword(String password) {
-		this.password = password;
-	}
+    @JsonIgnore
+    public String getPassword() {
+        return password;
+    }
 
-	public Collection<Role> getRoles() {
-		return roles;
-	}
+    @JsonProperty
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
-	public void setRoles(Collection<Role> roles) {
-		this.roles = roles;
-	}
+    public Collection<Role> getRoles() {
+        return roles;
+    }
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		User other = (User) obj;
-		return Objects.equals(email, other.email) && Objects.equals(id, other.id);
-	}
+    public void setRoles(Collection<Role> roles) {
+        this.roles = roles;
+    }
 
-	@Override
-	public String toString() {
-		return "User [id=" + id + ", password=" + password + ", name=" + name + ", lastName=" + lastName + ", email="
-				+ email + "]";
-	}
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        User other = (User) obj;
+        return Objects.equals(email, other.email) && Objects.equals(id, other.id);
+    }
+
+    @Override
+    public String toString() {
+        return "User [id=" + id + ", password=" + password + ", name=" + name + ", lastName=" + lastName + ", email="
+                + email + "]";
+    }
 
 }
